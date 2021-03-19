b718921f4d1ad2b53fdb87e64673d5090268f723
Ana Beatriz Costa de Oliveira <anabeatrizcosta.o@gmail.com>
2021-03-19 12:45:44 -0300

    config portas

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const cors = require('cors');
+// const cors = require('cors');
 const routes = require('./server/routes/index');
-const port = 3000;
+const port = process.env.port || 3000;
 const app = express();
 
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 app.use(routes);
 
