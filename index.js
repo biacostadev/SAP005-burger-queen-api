9713b6683dd45af340fced6027ea980cd6af1db5
Ana Beatriz Costa de Oliveira <anabeatrizcosta.o@gmail.com>
2021-03-19 12:40:51 -0300

    escondendo as chaves

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-const express = require('express')
-const routes = require('./server/routes/index')
-const app = express()
-const port = 3000
+const express = require('express');
+const cors = require('cors');
+const routes = require('./server/routes/index');
+const port = 3000;
+const app = express();
 
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 app.use(routes);
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
