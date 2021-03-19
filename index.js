4c88dce30ac29e69c7c6e827e1b0ae8925e9441c
Ana Beatriz Costa de Oliveira <anabeatrizcosta.o@gmail.com>
2021-03-19 12:49:51 -0300

    config portas

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require('express');
 // const cors = require('cors');
 const routes = require('./server/routes/index');
-const port = process.env.port || 3000;
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 // app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(port, () => {
+app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
