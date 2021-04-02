988c7df90c350231263c35df375e693634d0553d
Ana Beatriz Costa de Oliveira <anabeatrizcosta.o@gmail.com>
2021-04-02 00:20:28 -0300

    documentaçao swagger

--- swagger.js
+++ swagger.js
@@ -13,8 +13,7 @@ const doc = {
       "url": "https://github.com/biacostadev"
     }
   },
-  host: "testando-db.herokuapp.com",
-  basePath: "/",
+  host: "testando-db.herokuapp.com/",
   schemes: ['https'],
   consumes: ['application/json'],
   produces: ['application/json'],
