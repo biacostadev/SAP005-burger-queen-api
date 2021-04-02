df6598469993a32a321ef5cfe7b4e61f77343126
Ana Beatriz Costa de Oliveira <anabeatrizcosta.o@gmail.com>
2021-04-02 00:16:27 -0300

    documentaçao swagger

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,5 +1,4 @@
 const { Router } = require('express')
-const Users = require("./Users")
 const Auth = require("./Auth")
 const Products = require("./Products")
 const Orders = require("./Orders")
@@ -7,7 +6,6 @@ const Orders = require("./Orders")
 const router = Router()
 
 router.use('/auth', Auth);
-router.use('/users', Users);
 router.use('/products', Products);
 router.use('/orders', Orders);
 
